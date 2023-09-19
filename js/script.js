251329d96d0d00616507c3772d590abdec3c4384
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2023-09-19 13:09:30 +0200

    Slider from swiperjs.com installed and set

--- js/script.js
+++ js/script.js
@@ -49,3 +49,25 @@ accordionSingleHeaders.forEach((accordionSingleHeader, index) => {
     }
   });
 });
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters horizontal vs vertical
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
